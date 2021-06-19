b10bcbdd0e706b459a2fcf327ae499fb7d8af8c3
Abhishek Neupane <neupaneabhishek01@gmail.com>
2021-06-19 10:29:52 +0545

    Assignment on constructor and constructor overloading

---
+++ Assignments/z.Constructor/Constructor_Overloading.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class office  //creating multiple constructor.
+{
+private:
+    double totalwage, daily,bonus;
+
+public:
+   string name, address;
+    office() //default
+    {
+       cout<<"NAME AND ADDRESS :"<<endl;
+       cin>>name>>address;
+       cout<<" NAME: "<< name<<endl;
+       cout<<"ADDRESS: "<<address<<endl;
+    }
+
+    office( double x, double y)//parameterized
+    {
+        daily=x;
+        bonus=y;
+    }
+    int total()
+    {
+        totalwage=daily+bonus;
+        return totalwage;
+    }
+
+};
+int main()
+{
+    office w1;
+    office w2(50,30);
+    cout<<"TOTAL WAGE :"<<w2.total()<<endl;
+    return 0;
+}
---
+++ Assignments/z.Constructor/constructor_default.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+//constructor; no return type; special member function having same name as class ; (default, parameterized, copy)types
+class construct
+{
+
+public:
+    int a,b;
+
+    construct()//<-- constructor; default;
+    {
+        a=10;           // [Assignments on constructor overloading]
+        b=20;
+    }
+};
+int main()
+{
+    construct c; // yesma constructor is being called; object create huda constructor call(invoke) hunxa;
+    cout<<"a: "<<c.a<<endl;
+    cout<<"b: "<<c.b<<endl;
+    return 0;
+}
