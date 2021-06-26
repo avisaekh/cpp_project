c4635f6db33892ebbe20a566e331b28de3867585
Abhishek Neupane <neupaneabhishek01@gmail.com>
2021-06-26 12:25:28 +0545

    addition of two complex numbers,

---
+++ Lab works/Lab7_objects_As_arguments/addition_of_2_complexnum.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+
+    private:
+
+        double real;
+        double imaginary;
+
+    public:
+
+        void get_info(){
+            cout<<"Enter the real and imaginary value."<<endl;
+            cin>>real;
+            cin>>imaginary;
+        }
+
+        complex sum_complex(complex c1, complex c2){
+            complex add;
+            add.real= c1.real + c2.real;
+            add.imaginary = c1.imaginary + c2.imaginary;
+            return (add);
+        }
+
+        void dispaly(){
+            cout<<"The sum of the complex value is : "<<real<<"j "<<imaginary;
+        }
+
+};
+
+int main(){
+    complex c1,c2,c3;
+    c1.get_info();
+    c2.get_info();
+    c3=c3.sum_complex(c1,c2);
+    c3.dispaly();
+    return 0;
+}
