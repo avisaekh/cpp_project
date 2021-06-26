1f6ffd98bddaa1cde86277de001bfd09f9e6525a
Abhishek Neupane <neupaneabhishek01@gmail.com>
2021-06-26 11:57:08 +0545

    managed files and programs on constructor updated

---
+++ Lab works/Lab5_constructor/constructor_with_parameter.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class point
+{
+
+private:
+    int a,b;
+
+public:
+    point( int x, int y) //constructor with parameter/ parameterized constructor
+    {
+        a=x;
+        b=y;
+    }
+    int geta()
+    {
+        return a;                                                // (assignment on construction overloading.)
+    }
+    int getb()
+    {
+        return b;
+    }
+};
+int main()
+{
+    point p(5,10);  //object sanga parameter halera constructor call gareko. (just like function)/
+    cout<<"a: "<<p.geta()<<endl;
+    cout<<"b: "<<p.getb()<<endl;
+    return 0;
+}
